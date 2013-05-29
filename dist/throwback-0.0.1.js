13e6fe215cc45d8a0088914fbfc3b7461d46fa53
Stephen Young <young.steveo@gmail.com>
2013-05-29 15:29:41 -0400

    Initial boilerplate

---
+++ dist/throwback-0.0.1.js
@@ -0,0 +1,15 @@
+/**
+ * throwback v0.0.1 - 2013-05-29
+ * Retro Game Rendering Engine
+ *
+ * Copyright (c) 2013 Stephen Young <steve@rockgolem.com>
+ * Licensed MIT
+ */
+;(function(undefined){
+    "use strict";
+    if (typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports) {
+        // exports = module.exports = ProjectName;
+    } else {
+        // this.ProjectName = ProjectName;
+    }
+}).call(this);
