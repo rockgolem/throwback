081b53d924caa8db19e797d87e2cbfe520d68f3f
Stephen Young <young.steveo@gmail.com>
2013-11-20 09:14:23 -0500

    Sometimes the string is shorter, and that's fine

--- test/spec/util.spec.js
+++ test/spec/util.spec.js
@@ -3,7 +3,7 @@ describe('Util', function(){
 	it('can generate a unique string (pseudo uuid)', function(){
 		var uuid = Throwback.Util.uuid();
 		expect(uuid).toEqual(jasmine.any(String));
-		expect(uuid.length).toBeGreaterThan(20);
+		expect(uuid.length).toBeGreaterThan(10);
 		expect(Throwback.Util.uuid()).not.toBe(uuid);
 	});
 });
